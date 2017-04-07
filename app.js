99145957031b184bd4beab72749c7b911df4370c
PeterMRecords <petermennitirecords@gmail.com>
2017-04-07 17:18:17 -0400

    fixed 413 error

--- app.js
+++ app.js
@@ -28,10 +28,9 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ limit: '7mb', extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', index);
 app.use('/users', users);
 
