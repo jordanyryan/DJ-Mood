92f78b52820f1b5eae830282254ffd2fde657d6b
Jordany Rosas <jordanyryan@gmail.com>
2017-04-08 18:09:58 -0400

    added login through spotify

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/user/me', function(req, res, next) {
+  res.render('show', {})
+}
+
 module.exports = router;
+
+
