8589ddedfc30b0359f5ab759f86792a528d2f175
La-Keisha Towner <email": null>
2017-04-11 19:23:16 -0400

    updated routes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // load environment variables
-require('dotenv').config(); 
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -13,55 +13,50 @@ var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var User = require("./models/user");
 var $ = require('jquery');
-
-
+var expressValidator = require('express-validator')
 
 //Configure Spotify strategy
 
 passport.use(new SpotifyStrategy({
-  clientID: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/spotify/callback"
-}, function(accessToken, refreshToken, profile, done){
-  console.log(profile);
-  console.log("dsfsdfsad")
+    clientID: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/spotify/callback"
+}, function(accessToken, refreshToken, profile, done) {
 
-  if (profile.emails) {
-  User.findOneAndUpdate({
-    email: profile.emails[0].value
-  }, {
+    if (profile.emails) {
+        User.findOneAndUpdate({
+            email: profile.emails[0].value
+        }, {
 
-    name: profile.displayName,
-    email: profile.emails[0].value ,
-    username: profile.id
-
-  }, {
-    upsert: true
-  },
-  done);
-  } else {
-    var noEmailError = new Error("Your email privacy settings prevent you from signing into DJ Mood")
-    done(noEmailError, null);
-  }
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            username: profile.id,
+            preferences: []
+        }, {
+            upsert: true
+        }, done);
+    } else {
+        var noEmailError = new Error("Your email privacy settings prevent you from signing into DJ Mood")
+        done(noEmailError, null);
+    }
 
 }));
 
 
 // translate data structure for session storage, func with 2 args
-passport.serializeUser(function(user, done){
-  done(null, user.id);
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
 });
 
 // to read data you need to deserialize
-passport.deserializeUser(function(userId, done){
-  User.findById(userId, done);
+passport.deserializeUser(function(userId, done) {
+    User.findById(userId, done);
 });
 var routes = require('./routes/index');
 var auth = require('./routes/auth')
-var profile = require('./routes/profile')
+
 
 var app = express();
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -69,33 +64,33 @@ app.set('view engine', 'pug');
 
 // rendering the preferences for users based on Mood
 app.get('/preferences', (req, res) => {
-  res.render('preferences');
+    res.render('preferences');
 })
+// posting
+app.use(bodyParser.json());
+// app.post('/profile', function(req, res ) {
+//     // debugger;
+//     // console.log(req.body)
+//
+// })
 
-app.post('/preferences', function(req,res){
-  console.log(req.body);
-})
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({limit: '7mb',  extended: false }));
+app.use(bodyParser.urlencoded({limit: '7mb', extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// mongodb connection
 mongoose.connect(process.env.DB_URI);
 var db = mongoose.connection;
 
 //Session config for passport
 var sessionOptions = {
-  secret: process.env.DB_SECRET ,
-  resave: true,
-  saveUninitialized: true,
-  store: new MongoStore({
-    mongooseConnection: db
-  })
+    secret: process.env.DB_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({mongooseConnection: db})
 };
 
 app.use(session(sessionOptions))
@@ -113,12 +108,11 @@ app.use('/', routes);
 app.use('/auth', auth);
 
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -126,24 +120,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
-
 
 module.exports = app;
