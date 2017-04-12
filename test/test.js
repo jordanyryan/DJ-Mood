a8fda073e2a499f9bfcb94a7e699c8ba041d5cbc
La-Keisha Towner <email": null>
2017-04-12 15:20:48 -0400

    Added some content to tests

--- test/test.js
+++ test/test.js
@@ -6,7 +6,10 @@ describe("DJ Mood", function() {
     describe("GET /", function() {
         it("returns status code 200", function() {
             request.get(base_url, function(error, response, body) {
-
+              response.render('index', {
+                  title: 'Home',
+                  user: request.user
+              });
             });
         });
     });
@@ -14,7 +17,10 @@ describe("DJ Mood", function() {
     describe("GET /login", function() {
         it("returns status code 200", function() {
             request.get(base_url, function(error, response, body) {
-
+              response.render('login',{
+                title: 'Login',
+                user: request.user
+              })
             });
         });
     });
@@ -22,8 +28,26 @@ describe("DJ Mood", function() {
     describe("GET /profile", function() {
         it("returns status code 200", function() {
             request.get(base_url, function(error, response, body) {
+              if (request.user) {
+                  response.render('profile', {
+                      title: 'Profile',
+                      user: request.user
+                  });
+              } else {
+                  response.redirect('/login');
+              }
 
             });
         });
     });
+    describe("POST", function(){
+      it('returns status code 200', function(){
+        request.post(base_url, function(error, response, body){
+          let preferences = (Object.values(req.body))
+          User.update({_id: request.user.id }, { $set: { preferences: preferences}}, function(req, res){
+          })
+          resquest.redirect('/profile')
+        })
+      })
+    })
 });
