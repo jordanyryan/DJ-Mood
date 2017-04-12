498f6f486acd51c38e2f3b3d6f9a64edc414ad87
La-Keisha Towner <email": null>
2017-04-12 10:29:39 -0400

    Deleted trailing comments

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// load environment variables
 require('dotenv').config();
 var express = require('express');
 var path = require('path');
@@ -15,7 +14,7 @@ var User = require("./models/user");
 var $ = require('jquery');
 var expressValidator = require('express-validator')
 
-//Configure Spotify strategy
+
 
 passport.use(new SpotifyStrategy({
     clientID: process.env.SPOTIFY_CLIENT_ID,
@@ -43,12 +42,11 @@ passport.use(new SpotifyStrategy({
 }));
 
 
-// translate data structure for session storage, func with 2 args
+
 passport.serializeUser(function(user, done) {
     done(null, user.id);
 });
 
-// to read data you need to deserialize
 passport.deserializeUser(function(userId, done) {
     User.findById(userId, done);
 });
@@ -58,25 +56,17 @@ var auth = require('./routes/auth')
 
 var app = express();
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// rendering the preferences for users based on Mood
 app.get('/preferences', (req, res) => {
     res.render('preferences');
 })
-// posting
+
 app.use(bodyParser.json());
-// app.post('/profile', function(req, res ) {
-//     // debugger;
-//     // console.log(req.body)
-//
-// })
 
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({limit: '7mb', extended: false}));
 app.use(cookieParser());
@@ -95,30 +85,26 @@ var sessionOptions = {
 
 app.use(session(sessionOptions))
 
-//Initialize passport
+
 app.use(passport.initialize());
 
-// Restore users previous session
+
 app.use(passport.session());
 
-// mongo error
+
 db.on('error', console.error.bind(console, 'connection error:'));
 
 app.use('/', routes);
 app.use('/auth', auth);
 
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handlers
 
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
@@ -129,8 +115,7 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
+
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
